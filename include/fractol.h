aec6bc3b14ed02a6c01489b1e37cea5bd9830bf2
Hayase Fukushi <hfukushi@c1r26s9.42tokyo.jp>
2023-09-06 21:50:58 +0900

    mandatory

--- include/fractol.h
+++ include/fractol.h
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   fractol.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hfukushi <hfukushi@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/06 21:41:52 by hfukushi          #+#    #+#             */
+/*   Updated: 2023/09/06 21:47:22 by hfukushi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef FRACTOL_H
 # define FRACTOL_H
 
@@ -8,15 +20,15 @@
 # include "ft_printf.h"
 
 
-#define DestroyNotify			17
-#define StructureNotifyMask		(1L<<17)
-#define ButtonPress		4
-#define EnterWindowMask			(1L<<4)
+# define DestroyNotify 17
+# define StructureNotifyMask (1L<<17)
+# define ButtonPress	4
+# define EnterWindowMask			(1L<<4)
 
 
 typedef struct t_data
 {
-	void 	*mlx;
+	void	*mlx;
 	void	*mlx_win;
 	void	*img;
 	char	*addr;
@@ -26,24 +38,27 @@ typedef struct t_data
 	int		endian;
 	double	pixel;
 	double	size;
+	double	size_tmp;
 
-	double x_pos;
-	double y_pos;
+	double	x_pos;
+	double	y_pos;
 
-} t_data;
+	int		color_change;
+
+}	t_data;
 
 typedef struct t_complex
 {
-	double real;
-	double imag;
-} t_complex;
+	double	real;
+	double	imag;
+}	t_complex;
 
-int deal_key(int key, t_data *data);
-int deal_window_cross(t_data *data);
-int deal_mouse_pointer(int button, int  x, int y, t_data *data);
-int make_julia(t_data *data);
-int mandel_caluculate(t_complex complex);
-int make_mandel(t_data *data);
+int		deal_key(int key, t_data *data);
+int		deal_window_cross(t_data *data);
+int		deal_mouse_pointer(int button, int x, int y, t_data *data);
+int		make_julia(t_data *data);
+int		mandel_caluculate(t_complex complex);
+int		make_mandel(t_data *data);
 void	my_mlx_pixel_put(t_data *data, int x, int y, int color);
 
 #endif
--- julia_deal_mlx.c
+++ julia_deal_mlx.c
@@ -1,58 +1,62 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   julia_deal_mlx.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hfukushi <hfukushi@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/06 20:34:44 by hfukushi          #+#    #+#             */
+/*   Updated: 2023/09/06 21:36:14 by hfukushi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "fractol.h"
-#include <stdio.h>
 
 int	deal_key(int key, t_data *data)
 {
 	(void )data;
 	if (key == 53)
-	{
-	printf("OK\n");
-	exit (0);
-	}
-	else
-	printf("NO\n");
+		exit (0);
 	return (0);
 }
 
 int	deal_window_cross(t_data *data)
 {
 	(void )data;
-	printf("cross\n");
 	exit(0);
 	return (0);
 }
 
-int deal_mouse_pointer(int button, int  x, int y, t_data *data)
+void	zoom_in_out(t_data *data, int button)
 {
-	if(button  == 1)
+	if (button == 5)
+		data->size *= 0.9;
+	else if (button == 4)
+		data->size *= 1.1111;
+	mlx_destroy_image(data->mlx, data->img);
+	data->img = mlx_new_image(data->mlx, 1000, 1000);
+	data->addr = mlx_get_data_addr (data->img, &(data->bits_per_pixel),
+			&(data->line_length), &(data->endian));
+	make_julia(data);
+}
+
+int	deal_mouse_pointer(int button, int x, int y, t_data *data)
+{
+	if (button == 1)
 	{
-		mlx_destroy_image(data->mlx,data->img);
+		data->size = data->size_tmp;
+		mlx_destroy_image(data->mlx, data->img);
 		data->img = mlx_new_image(data->mlx, 1000, 1000);
-		data->addr = mlx_get_data_addr (data->img, &(data->bits_per_pixel),&(data->line_length), &(data->endian));
-		
+		data->addr = mlx_get_data_addr (data->img, &(data->bits_per_pixel),
+				&(data->line_length), &(data->endian));
 		mlx_mouse_get_pos(data->mlx_win, &x, &y);
-		printf("x = [%f], y = [%f]\n", (double)x, (double)y);
 		data->x_pos = (double)x *(data->size / data->pixel) - (data->size / 2);
-		data->y_pos = -((double)y *(data->size / data->pixel) - (data->size / 2));
-		printf("x = [%f], y = [%f]\n",data->x_pos, data->y_pos);
-		if( data->x_pos <=2 && data->y_pos <= 2 )
+		data->y_pos = -((double)y *(data->size / data->pixel)
+				-(data->size / 2));
+		if (data->x_pos <= 2 && data->y_pos <= 2)
 			make_julia(data);
 	}
-	if(button == 5)
-	{
-		mlx_destroy_image(data->mlx,data->img);
-		data->size *= 0.9;
-		data->img = mlx_new_image(data->mlx, 1000, 1000);
-		data->addr = mlx_get_data_addr (data->img, &(data->bits_per_pixel),&(data->line_length), &(data->endian));
-		make_julia(data);
-	}
-	else if(button == 4)
-	{
-		mlx_destroy_image(data->mlx,data->img);
-		data->size *= 1.111111;
-		data->img = mlx_new_image(data->mlx, 1000, 1000);
-		data->addr = mlx_get_data_addr (data->img, &(data->bits_per_pixel),&(data->line_length), &(data->endian));
-		make_julia(data);
-	}
+	if (button == 5 || button == 4)
+		zoom_in_out(data, button);
 	return (0);
 }
